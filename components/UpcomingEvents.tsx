eeff33175846717995a5042cf9db3e60ea7ab204
Amnafazal2004 <fazalamna666@gmail.com>
2026-02-13 08:52:30 +0500

    timer fixed

--- components/UpcomingEvents.tsx
+++ components/UpcomingEvents.tsx
@@ -66,7 +66,7 @@ const UpcomingEvents = () => {
               initial={{ opacity: 0, x: 50 }}
               animate={{ opacity: 1, x: 0 }}
               exit={{ opacity: 0, x: -50 }}
-              transition={{ duration: 0.5, ease: "easeInOut" }}
+              transition={{ duration: 0.2, ease: "easeInOut" }}
             >
               {currentBatch.map((event) => (
                 <div
